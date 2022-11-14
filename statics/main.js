0e2d3dc616c763a776550a9988ce266c7ed28d39
Shivam Singh <sd.shivam.00@gmail.com>
2022-11-14 23:35:23 +0530

    about timetable page

--- statics/main.js
+++ statics/main.js
@@ -13,3 +13,17 @@ function logbox(ids){
         }
     }
 }
+
+function contact_save(){
+    let name=document.getElementsByClassName('con-inp')[0].value;
+    let email=document.getElementsByClassName('con-inp')[1].value;
+    let number=document.getElementsByClassName('con-inp')[2].value;
+    let msg=document.getElementsByClassName('con-inp')[3].value;
+    if(name==''){
+        document.getElementById('war-con').style.display='block';
+    }
+    else{
+        console.log(name,email,number,msg);
+        document.getElementById('contact-form').innerHTML ='<span style="display: block;color: white;font-size: 25px;margin-top: 120px;margin-left: -100px;text-shadow: -1px 5px 7px rebeccapurple;">Thanks For contacting Us! <br> We will get back to you soon.</span>';
+    }
+}
